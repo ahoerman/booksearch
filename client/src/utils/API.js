5ab8d90ae9eeebc48a33ec4a96d04b3a91f79b09
A Hoerman <awhoerman@me.com>
2020-05-25 15:01:57 -0400

    remove key

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,11 @@
+require("dotenv").config({ path: __dirname + "/../"});
 import axios from "axios";
 
 // The getBooks method retrieves Books from the server
 // It accepts a "query" or term to search the recipe api for
 export default {
   getBooks: function(query) {
-    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&download=epub&key=AIzaSyAnHeoV3WcfOOM544C-loQb4ZbdEjy88Iw`)
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&download=epub&key=${process.env.google_api}`)
     .then((response) => {
       console.log(response);
   })
